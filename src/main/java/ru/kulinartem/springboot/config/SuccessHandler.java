c399a62ce1d65132b9fd3a6efa02b35ce576cb1a
Artem Kulin <kulinartem@gmail.com>
2021-03-11 13:42:44 +0300

    modify SuccessHandler

--- src/main/java/ru/kulinartem/springboot/config/SuccessHandler.java
+++ src/main/java/ru/kulinartem/springboot/config/SuccessHandler.java
@@ -6,10 +6,8 @@ import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
 import ru.kulinartem.springboot.repository.UserRepository;
-import ru.kulinartem.springboot.service.UserService;
 
 
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
@@ -26,9 +24,9 @@ public class SuccessHandler implements AuthenticationSuccessHandler {
     }
 
     @Override
-    public void onAuthenticationSuccess(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Authentication authentication) throws IOException, ServletException {
+    public void onAuthenticationSuccess(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Authentication authentication) throws IOException {
         Set<String> roles = AuthorityUtils.authorityListToSet(authentication.getAuthorities());
-        String name = httpServletRequest.getParameter("email");
+        String name = httpServletRequest.getParameter("username");
         long id = repository.findByEmail(name).get().getId();
         if (roles.contains("ROLE_ADMIN")) {
             httpServletResponse.sendRedirect("/admin/");

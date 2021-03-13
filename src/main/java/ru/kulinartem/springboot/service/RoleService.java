604eb387e40a7803111a87ec9133a95294375825
Artem Kulin <kulinartem@gmail.com>
2021-03-13 21:45:40 +0300

    add RoleService

---
+++ src/main/java/ru/kulinartem/springboot/service/RoleService.java
@@ -0,0 +1,10 @@
+package ru.kulinartem.springboot.service;
+
+import ru.kulinartem.springboot.entity.Role;
+
+
+import java.util.List;
+
+public interface RoleService {
+    public List<Role> getAllItems();
+}

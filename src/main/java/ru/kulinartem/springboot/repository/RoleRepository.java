a79dcae70372185c54d5f6cb2448a8b105ebdeb3
Artem Kulin <kulinartem@gmail.com>
2021-03-15 15:03:20 +0300

    delete useless role repo % service

--- src/main/java/ru/kulinartem/springboot/repository/RoleRepository.java
+++
@@ -1,7 +0,0 @@
-package ru.kulinartem.springboot.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import ru.kulinartem.springboot.entity.Role;
-
-public interface RoleRepository extends JpaRepository<Role, Long> {
-}
